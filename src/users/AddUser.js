d32437849520c7c16f31b33b2b4ff51ebf10c14e
jeongyong <byunjy@chainverse.co.kr>
2023-07-14 17:26:09 +0900

    work2 07-14

---
+++ src/users/AddUser.js
@@ -0,0 +1,10 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+export default function AddUser() {
+    const [user,setUser]=useState();
+    
+    return (
+    <div>AddUser</div>
+    )
+}
